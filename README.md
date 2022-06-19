5173d4c2b46b7e1f9b3010ce644c1a20a1064e01
Sumit <diysumit@gmail.com>
2022-06-20 03:00:26 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# System Setup
+
+### **Bash Script to setup new linux systems for my work, works on windows too when run with bash shell**
---
+++ setup.sh
@@ -0,0 +1,34 @@
+# Setting up OS
+distro_family=$(cat /etc/os-release | grep "^ID_LIKE" | cut -b 9-20)
+if [ ! -z $distro_family ]; then
+	if [ $distro_family=='arch' ]; then
+		echo "==========================INSTALLING DEPENDENCIES=========================="
+		sudo pacman -Syyu --needed base-devel git wget yajl python3 python-is-python3 code snapd telegram-desktop discord gnome-tweaks mscorefonts-installer
+		echo ""
+		echo "=============================REMOVING UNINSTALLED============================="
+		sudo pacman -Sc
+		echo ""
+		echo "==================================UNUSED PACKAGES=================================="
+		sudo pacman -Qtdq
+	elif [ $distro_family=='debian' ]; then
+		echo ""
+		echo "============================UPDATING && UPGRADING============================"
+		sudo apt-get update && apt-get upgrade
+		echo ""
+		echo "==========================INSTALLING DEPENDENCIES=========================="
+		sudo apt-get install build-essential python3 python-is-python3 git code snapd telegram-desktop discord gnome-tweaks
+		echo ""
+		echo "===================================HOUSE CLEANING==================================="
+		sudo apt autoclean && apt-get autoremove
+	fi
+fi
+
+# Installing python packages for my work
+echo ""
+echo "==================================PYTHON DEPENDENCIES=================================="
+python -m ensurepip install
+pip install --upgrade google-compute-engine google-cloud-storage matplotlib tensorflow sklearn plotly pandas pyspark numpy gcloud kaggle torch docker kubernetes jupyter notebook anaconda keras bs4 scipy scrapy seaborn theano mahotas pillow simpleitk requests selenium pytest gsutil psutil
+
+echo ""
+echo "================================REMOVING CACHED PACKAGES================================"
+pip cache purge
