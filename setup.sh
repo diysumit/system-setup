a8c7e0daf7ded11c5b23b3a70a8ac0658f153a57
Sumit <diysumit@gmail.com>
2022-06-24 06:23:40 +0530

    Added zsh configuration to Manjaro, removed some packages and formatted output.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,17 @@
 # Setting up OS
+RED='\033[0;31m'
+GREEN="\033[2;32m"
+BLUE='\033[1;34m'
+NC='\033[0m' # No Color
+BOLD=$(tput bold)
+NORM=$(tput sgr0)
 distro_family=$(cat /etc/os-release | grep "^ID_LIKE" | cut -b 9-30)
 if [[ ! -z $distro_family ]]; then
 	if [[ $distro_family == *'arch'* ]]; then
 		echo "==========================INSTALLING DEPENDENCIES=========================="
 		# * enable AUR packages for pamac
 		sudo sed -Ei 'EnableAUR/s/^#//' /etc/pamac.conf
-		sudo pacman -Syyu --needed base-devel git wget yajl python3 code snapd telegram-desktop discord gnome-tweaks python-pip neofetch postgresql xclip xsel opencl-mesa opencl-amd opencl-legacy-amdgpu-pro rocm-opencl-runtime amdapp-sdk
+		sudo pacman -Syyu --needed base-devel git wget yajl python3 python-pip python-wheel code snapd telegram-desktop discord gnome-tweaks python-pip neofetch postgresql xclip xsel opencl-mesa
 		echo ""
 		echo "=============================REMOVING UNINSTALLED============================="
 		sudo pacman -Sc
@@ -18,7 +24,7 @@ if [[ ! -z $distro_family ]]; then
 		sudo apt-get update && apt-get upgrade
 		echo ""
 		echo "==========================INSTALLING DEPENDENCIES=========================="
-		sudo apt-get install build-essential python3 python-is-python3 git code snapd telegram-desktop discord gnome-tweaks python3-pip neofetch zsh git-extras postgresql postgresql-contrib xclip xsel tensorman docker gnome-session
+		sudo apt-get install build-essential python3 python-is-python3 python-wheel git code snapd telegram-desktop discord gnome-tweaks python3-pip neofetch zsh git-extras postgresql postgresql-contrib xclip xsel tensorman docker gnome-session
 		# * adding tensorman to docker group
 		sudo usermod -aG docker $USER
 		# * adding kernal parameter 
@@ -38,18 +44,16 @@ echo ""
 echo "================================REMOVING CACHED PACKAGES================================"
 pip cache purge
 
-if [[ $distro_family == *"debian"* ]]; then
-	# * installing oh-my-zsh
-	echo "======================================SETTING UP OH-MY-ZSH======================================"
-	sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-	export PATH=$PATH:/home/$USER/.local/bin
-	gdown --fuzzy https://drive.google.com/file/d/1MVR3yxRAJ9Oq_msjA9kB9izlXM55LCZ2/view?usp=sharing -O $HOME/.zshrc
-	sed -i 's/diysumit/$USER/' $HOME/.zshrc
-	chsh -s $(which zsh) $(whoami)
-	echo "use to install zsh-autosuggestions: git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions"
-	echo "use to install zsh-syntax-highlighting: git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
-	echo "gdown is not added to path, add /home/\$USER/.local/bin to path"
-fi
+# * installing oh-my-zsh
+echo "======================================SETTING UP OH-MY-ZSH======================================"
+sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+export PATH=$PATH:/home/$USER/.local/bin
+gdown --fuzzy https://drive.google.com/file/d/1MVR3yxRAJ9Oq_msjA9kB9izlXM55LCZ2/view?usp=sharing -O $HOME/.zshrc
+sed -i 's/diysumit/$USER/' $HOME/.zshrc
+chsh -s $(which zsh) $(whoami)
+echo -e "${RED}${BOLD}install zsh-autosuggestions: ${BLUE}git clone https://github.com/zsh-users/zsh-autosuggestions.git \$ZSH_CUSTOM/plugins/zsh-autosuggestions${NC}${NORM}"
+echo -e "${RED}${BOLD}install zsh-syntax-highlighting: ${BLUE}git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \$ZSH_CUSTOM/plugins/zsh-syntax-highlighting${NC}${NORM}"
+echo -e "${RED}${BOLD}gdown is not added to path, add ${BLUE}/home/\$USER/.local/bin to path${NC}${NORM}"
 
 # * settting up kaggle api
 mkdir -p $HOME/.kaggle
@@ -72,5 +76,5 @@ cd fonts
 cd ..
 rm -rf fonts
 
-echo "Login again for changes to take effect"
+echo -e "${GREEN}Login again for changes to take effect${NC}"
 zsh
